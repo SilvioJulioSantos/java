2de2e5614b27b4d6d299b699610e0c874bfeb420
Silvio Julio <silvio.dossantos@mercadolivre.com>
2022-11-01 09:45:34 -0300

    Atividade3 Do..While prática – JAVA6 – Laços de Repetição foi corrigida e atualizada.

--- aula-03/src/generation/meliSilvioJulio/AtividadeDoWhile3.java
+++ aula-03/src/generation/meliSilvioJulio/AtividadeDoWhile3.java
@@ -3,48 +3,32 @@ package generation.meliSilvioJulio;
 import java.util.Scanner;
 
 public class AtividadeDoWhile3 {
-    /**
-     *
-     * Escreva um algoritmo em Java, que leia números inteiros via
-     * teclado, até que o número zero seja digitado. Ao final,
-     * mostre na tela a soma de todos os números digitados,
-     * que sejam positivos. Veja o exemplo abaixo:
-     *
-     * @param args
-     */
+
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
 
+        int numeros = 0,contador=1;
+        int somaNumerosPositivo =0;
 
-        //Vetor que armazena 3 números
-        //int [] numeros = new int[10];
-        int numeros =0 ;
-        int somaNumerosPositivoa = 0 , totalSoma = 0;
-        int contador = 1;
-
+        Scanner scanner = new Scanner(System.in);
         do {
-            //Entrada de dados
-            System.out.println("Digite " + contador + "º número");
-             numeros = entrada.nextInt();
-             contador++;
-             //somaNumerosPositivoa = numeros;
-
-
-
+            System.out.println("Digite o " + contador + "° numero: ");
+            numeros = scanner.nextInt();
+            contador++;
             if (numeros > 0) {
-
-              somaNumerosPositivoa =+numeros;
-
+                somaNumerosPositivo += numeros;
             }
-
 
         } while (numeros != 0);
 
         System.out.println("======================== Resultado final   =========================\n ");
-        //System.out.println("A soma dos números positivos é: " + somaNumerosPositivoa);
-        System.out.println("A soma dos números positivos é: " + somaNumerosPositivoa);
+        System.out.println("================================================================");
+        System.out.println("A soma dos números positivos é: " + somaNumerosPositivo);
+        System.out.println("================================================================");
+
         System.out.println("\nPrograma finalizado!!");
         entrada.close();
+
     }
 }
